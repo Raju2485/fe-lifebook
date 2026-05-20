ac5a82095c7b6925b7062e76fe7db9556bd0251b
Bheemaraju Bojja <bheemaraju.bojja@lexyslabs.com>
2026-05-20 10:10:23 +0530

    accessing accessToken issue fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getLocalStorage } from '../utils/localStorage'
 
-const user = getLocalStorage('user')
-const authToken = user?.accessToken ?? false
-
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:3000/api/v1/',
     prepareHeaders: (headers) => {
-      // If we have a token set in state, let's assume that we should be passing it.
+      const user = getLocalStorage('user')
+      const authToken = user?.accessToken
       if (authToken) {
         headers.set('authorization', `Bearer ${authToken}`)
       }

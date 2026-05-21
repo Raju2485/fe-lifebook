66b38674dc73c3951491b5218460427cfe17993a
Bheemaraju Bojja <bheemaraju.bojja@lexyslabs.com>
2026-05-21 16:57:15 +0530

    header alignment issue fixed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
 import { Typography } from 'antd'
 import { Link, useNavigate } from 'react-router-dom'
 import { Header } from '../features/header'
-import { Layout } from 'antd'
-const { Header: HeaderTag, Content } = Layout
 
 export function NotFound() {
   const navigate = useNavigate()
 
   return (
-    <Layout>
-      <HeaderTag>
-        <Header />
-      </HeaderTag>
-      <Content>
+    <div className="app-layout">
+      <Header />
+      <main className="app-layout__content">
         <div className="text-center">
           <Typography.Title level={3}>Page Not Found</Typography.Title>
           <Link onClick={() => navigate(-1)}>Back</Link>
           <br />
           <Link to="/dashboard">Home</Link>
-        </div>{' '}
-      </Content>
-    </Layout>
+        </div>
+      </main>
+    </div>
   )
 }
